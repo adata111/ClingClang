198522ccb8b8ea0cfaa55a99fb626a5e032dba59
Ahana Datta <ahana.datta.ravenclaw@gmail.com>
2023-04-12 03:30:18 +0530

    prelude-and-conclude kinda done

--- prelude-and-conclusion.rkt
+++ prelude-and-conclusion.rkt
@@ -14,7 +14,7 @@
 ;; prelude-and-conclusion : x86 -> x86
 (define (prelude-and-conclusion p)
   
-  (define (make-prelude-conclusion body-dict info)
+  (define (make-prelude-conclusion body-dict info fun-name)
 
     (define push-used-callees                             ; construct the instructions to push all the used callee-saved registers in the prelude
       (for/fold ([push-callee-instrs '()])                               
@@ -33,7 +33,7 @@
     (define main-body       (Block '() (append            ; update rbp to rsp, push used callee-saved registers, move rsp to allocate stack space for all variables, jump to start
                                         (list (Instr 'pushq (list (Reg 'rbp))) (Instr 'movq (list (Reg 'rsp) (Reg 'rbp))))
                                         push-used-callees
-                                        (list (Instr 'subq (list (Imm (dict-ref info 'stack-space)) (Reg 'rsp))) (Jmp 'start))))
+                                        (list (Instr 'subq (list (Imm (dict-ref info 'stack-space)) (Reg 'rsp))) (Jmp (symbol-append fun-name 'start)))))
     )
 
     (define conclusion-body (Block '() (append      ; move rsp back to the rbp of this frame, pop all used callee-saved registers, get the rbp of previous frame, return
@@ -41,11 +41,56 @@
                                         pop-used-callees
                                         (list (Instr 'popq (list (Reg 'rbp))) (Retq))))
     )
+    (define (expand-tail-jmp target)
+      (append
+        (list (Instr 'addq (list (Imm (dict-ref info 'stack-space)) (Reg 'rsp))))
+        pop-used-callees
+        (list (Instr 'popq (list (Reg 'rbp))) (IndirectJmp target)))
+    )
 
-    (dict-set (dict-set body-dict 'main main-body) 'conclusion conclusion-body )
+    (define (process-block block)                                 ; process each line and change tailjmp instruction
+      (match block
+        [(Block info block-lines) (Block '() (                    ; remove the info section of the block
+            for/fold ([new-block-lines '()]) 
+                      ([line block-lines]) 
+                      (append new-block-lines (match line
+                                          [(TailJmp reg arity) (expand-tail-jmp reg)]
+                                          [_ (list line)]))))])  
+    )
+
+    (define process-body 
+      (for/fold ([new-body-dict '()])                               ; for each (label, block), patch each block
+              ([(label block) (in-dict body-dict)]) 
+              (dict-set new-body-dict label (patch-block block))
+      )
+      ; (for/fold ([new-body '()]) ([(k v) (in-dict body-dict)])
+      ;           (dict-set new-body k
+      ;             (match v
+      ;               [(Block info block-lines) 
+      ;                 (Block '() (                    ;
+      ;                     for/fold ([new-block-lines '()]) 
+      ;                               ([line block-lines]) 
+      ;                               (append new-block-lines 
+      ;                                   (match line
+      ;                                     [(TailJmp reg arity) (expand-tail-jmp reg)]
+      ;                                     [_ (list line)]))))]
+      ;             )))
+    )
+
+    
+    (dict-set (dict-set process-body fun-name main-body) (symbol-append fun-name 'conclusion) conclusion-body )
+  )
+
+  (define (make-prelude-conclusion-def def)
+    (match def
+      [(Def fun-name param-list ret-type fun-info fun-body)
+        (make-prelude-conclusion fun-body fun-info fun-name)]
+    )
   )
 
   (match p
-    [(X86Program info body) (X86Program info (make-prelude-conclusion body info))]
+    [(ProgramDefs info defs) 
+        (X86Program info (for/fold ([new-defs '()]) 
+                                    ([def defs]) (append new-defs (make-prelude-conclusion-def def))))]
   )
 )
